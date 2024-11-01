9916670fe8d960288ac20f80efe604867c214071
saliyvlad <saliy.vlad.jr@gmail.com>
2024-11-01 09:06:50 +0300

    Update Лаба 8.py

--- Лаба 8.py
+++ Лаба 8.py
@@ -159,4 +159,30 @@
 
 ## Применяем функцию к каждому слову и выводим результат
 #result = [abbreviate(word) for word in words]
-#print(result)
+#print(res
+def luhn_algorithm(card_number):
+    def digits_of(n):
+        return [int(d) for d in str(n)]
+    
+    digits = digits_of(card_number)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
+    
+    checksum = sum(odd_digits)
+    
+    for d in even_digits:
+        checksum += sum(digits_of(d * 2))
+    
+    return checksum % 10 == 0
+
+# Входные данные
+card_numbers = [
+    "4276440013361511",
+    "4276440013361512",
+    "42761336512"
+]
+
+# Вывод результатов
+for number in card_numbers:
+    is_valid = luhn_algorithm(number)
+    print(f"{number}: {'Корректный номер' if is_valid else 'Некорректный номер'}")
