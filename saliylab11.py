66d6ac03a9355c8ccadf062c0aa9dba608f7a735
saliyvlad <saliy.vlad.jr@gmail.com>
2024-11-29 10:42:38 +0700

    Update saliylab11.py

--- saliylab11.py
+++ saliylab11.py
@@ -44,5 +44,43 @@
 #     for k in j:
 #         print(k,"@", i,sep="")
 
+# 4
+# import PySimpleGUI as mp
+# import random
 
+# # Определяем тему оформления
+# mp.theme('LightBlue')
+
+# # Задаем макет интерфейса
+# layout = [
+#     [mp.Text('Введите нижнюю границу:'), mp.InputText(key='lower_bound')],
+#     [mp.Text('Введите верхнюю границу:'), mp.InputText(key='upper_bound')],
+#     [mp.Text('Случайное число:'), mp.InputText(key='random_number', disabled=True)],
+#     [mp.Button('Сгенерировать')],
+#     [mp.Image(filename='C:/Users/saliy_03jey6u/Desktop/3de9c7cb6baa455bbee4e4f64b4cbe10.jpg')]  # Укажите путь к вашему изображению
+# ]
+
+# # Создаем окно
+# window = mp.Window('Генератор случайных чисел', layout)
+
+# # Основной цикл программы
+# while True:
+#     event, values = window.read()
+    
+#     if event == mp.WINDOW_CLOSED:
+#         break
+#     elif event == 'Сгенерировать':
+#         try:
+#             lower_bound = int(values['lower_bound'])
+#             upper_bound = int(values['upper_bound'])
+#             if lower_bound < upper_bound:
+#                 random_number = random.randint(lower_bound, upper_bound)
+#                 window['random_number'].update(random_number)
+#             else:
+#                 mp.popup_error('Нижняя граница должна быть меньше верхней границы!')
+#         except ValueError:
+#             mp.popup_error('Пожалуйста, введите целые числа!')
+
+# # Закрываем окно
+# window.close()
             
